e15433eac51cebad1e912bcd6d0d66b339546b80
Peter <peterkimbeghi@gmail.com>
2023-02-21 13:22:14 -0800

    Task 0 done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * File: 0-putchar.c
+ * name : peter kimbeghi
+ */
+
+int main(void)
+{
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
