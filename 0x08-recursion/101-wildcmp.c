e9e1ff29571797967be060c9868f20a24c9a5606
Peter Kimbeghi <Peterkimbeghi@gmail.com>
2023-03-09 18:25:05 -0800

    followed the betty coding style

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,17 +9,17 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s2 == '*')
-    {
-        if (*s1 == '\0')
-            return (wildcmp(s1, s2 + 1));
-        else
-            return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-    }
-    if (*s1 == '\0')
-        return (*s2 == '\0');
-    if (*s1 == *s2)
-        return (wildcmp(s1 + 1, s2 + 1));
-    return (0);
+	if (*s2 == '*')
+	{
+		if (*s1 == '\0')
+			return (wildcmp(s1, s2 + 1));
+		else
+			return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+	if (*s1 == '\0')
+		return (*s2 == '\0');
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
 }
 
